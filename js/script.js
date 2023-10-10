54453a1c724511e4a404681f501ab63e745970fa
mdhaka1 <dhamanish123@gmail.com>
2023-10-10 14:35:54 +0530

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,73 @@
+let SearchForm=document.querySelector('.search-form');
+document.querySelector('#search-btn').onclick=()=>
+{
+    SearchForm.classList.toggle('active');
+    
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navBar.classList.remove('active');
+    
+
+}
+let shoppingCart=document.querySelector('.shopping-cart');
+document.querySelector('#cart-btn').onclick=()=>
+{
+    shoppingCart.classList.toggle('active');
+    SearchForm.classList.remove('active');
+    loginForm.classList.remove('active');
+    navBar.classList.remove('active');
+
+}
+let loginForm=document.querySelector('.login-form');
+document.querySelector('#login-btn').onclick=()=>
+{
+    loginForm.classList.toggle('active');
+    SearchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    navBar.classList.remove('active');
+
+}
+
+let navBar=document.querySelector('.navbar');
+document.querySelector('#menu-btn').onclick=()=>
+{
+    navBar.classList.toggle('active');
+    
+    SearchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+
+}
+
+window.onscroll = () =>
+{
+   
+    SearchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navBar.classList.remove('active');
+    
+
+}
+
+var swiper = new Swiper(".product-slider", {
+    loop: true,
+    spaceBetween: 20,
+
+    autoplay: {
+        delay: 7050,
+        disableOnInteraction: false,
+    },
+
+    breakpoints:{
+        0:{
+            slidesPerView: 1,
+        },
+        768:{
+            slidesPerView: 2,
+        },
+        1020:{
+            slidesPerView: 2,
+        },
+    },
+});
